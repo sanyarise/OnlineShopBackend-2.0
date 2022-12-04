9ecac2939e0f6fa7a1f48abc8bfd88e228053636
Kirill Ivanov <xfiendx4life@gmail.com>
2022-12-05 00:27:46 +0200

    cart repo

--- internal/repository/cart_repo.go
+++ internal/repository/cart_repo.go
@@ -1,1 +1,94 @@
 package repository
+
+import (
+	"OnlineShopBackend/internal/models"
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4"
+	"go.uber.org/zap"
+)
+
+type cart struct {
+	storage Storage
+	logger  *zap.SugaredLogger
+}
+
+var _ CartStore = (*cart)(nil)
+
+func NewCartStore(storage Storage, logger *zap.SugaredLogger) CartStore {
+	return &cart{
+		storage: storage,
+		logger:  logger,
+	}
+}
+
+func (c *cart) Create(ctx context.Context, cart *models.Cart) (*models.Cart, error) {
+	select {
+	case <-ctx.Done():
+		return nil, fmt.Errorf("context closed")
+	default:
+		pool := c.storage.GetPool()
+		row := pool.QueryRow(ctx, `INSERT INTO cart (user_id, expire_at) VALUES ($1, $2) RETURNING id`,
+			cart.UserID, cart.ExpireAt)
+		err := row.Scan(cart.ID)
+		if err != nil {
+			return nil, fmt.Errorf("can't create cart object: %w", err)
+		}
+		return cart, nil
+	}
+}
+
+// ? Maybe add to item
+func (c *cart) AddItemToCart(ctx context.Context, cart *models.Cart, item *models.Item) error {
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("context closed")
+	default:
+		pool := c.storage.GetPool()
+		_, err := pool.Exec(ctx, `INSERT INTO cart_items (cart_id, item_id) VALUES ($1, $2)`, cart.ID, item.Id)
+		if err != nil {
+			return fmt.Errorf("can't add item to cart: %w", err)
+		}
+		return nil
+	}
+}
+
+func (c *cart) DeleteCart(ctx context.Context, cart *models.Cart) error {
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("context closed")
+	default:
+		pool := c.storage.GetPool()
+		tx, err := pool.BeginTx(ctx, pgx.TxOptions{})
+		defer func() {
+			if err != nil {
+				tx.Rollback(ctx)
+			} else {
+				tx.Commit(ctx)
+			}
+		}()
+		_, err = tx.Exec(ctx, `DELETE FROM cart_items WHERE cart_id=$1`, cart.ID)
+		if err != nil {
+			return fmt.Errorf("can't delete cart items from cart: %w", err)
+		}
+		_, err = tx.Exec(ctx, `DELETE FROM carts WHERE id=$1`, cart.ID)
+		if err != nil {
+			return fmt.Errorf("can't delete cart: %w", err)
+		}
+		return nil
+	}
+}
+func (c *cart) DeleteItemFromCart(ctx context.Context, cart *models.Cart, item *models.Item) error {
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("context closed")
+	default:
+		pool := c.storage.GetPool()
+		_, err := pool.Exec(ctx, `DELETE FROM cart_irems WHERE item_id=$1 AND cart_id=$2`, item.Id, cart.ID)
+		if err != nil {
+			return fmt.Errorf("can't delete item from cart: %w", err)
+		}
+		return nil
+	}
+}
