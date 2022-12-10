d7f49ab6cb5964618097d13900eecb5021a0eba5
sanyarise <san_rise@list.ru>
2022-12-10 22:14:48 +0300

    feat: update category handlers and items

--- internal/handlers/category_handlers.go
+++ internal/handlers/category_handlers.go
@@ -2,10 +2,24 @@ package handlers
 
 import (
 	"OnlineShopBackend/internal/models"
+	"OnlineShopBackend/internal/usecase"
 	"context"
 
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
+
+var _ ICategoryHandlers = &CategoryHandlers{}
+
+type CategoryHandlers struct {
+	usecase usecase.ICategoryUsecase
+	logger  *zap.Logger
+}
+
+func NewCategoryHandlers(usecase usecase.ICategoryUsecase, logger *zap.Logger) *CategoryHandlers {
+	return &CategoryHandlers{usecase: usecase, logger: logger}
+}
+
 // Category is struct for DTO
 type Category struct {
 	Id          string `json:"id,omitempty"`
@@ -14,13 +28,13 @@ type Category struct {
 }
 
 // CreateCategory transform Category to models.Category and call usecase CreateCategory
-func (handlers *Handlers) CreateCategory(ctx context.Context, category Category) (uuid.UUID, error) {
+func (handlers *CategoryHandlers) CreateCategory(ctx context.Context, category Category) (uuid.UUID, error) {
 	handlers.logger.Debug("Enter in handlers CreateCategory()")
 	newCategory := &models.Category{
 		Name:        category.Name,
 		Description: category.Description,
 	}
-	id, err := handlers.repo.CreateCategory(ctx, newCategory)
+	id, err := handlers.usecase.CreateCategory(ctx, newCategory)
 	if err != nil {
 		return id, err
 	}
@@ -28,10 +42,10 @@ func (handlers *Handlers) CreateCategory(ctx context.Context, category Category)
 }
 
 // GetCategoryList returns list of all categories
-func (handlers *Handlers) GetCategoryList(ctx context.Context) ([]Category, error) {
+func (handlers *CategoryHandlers) GetCategoryList(ctx context.Context) ([]Category, error) {
 	handlers.logger.Debug("Enter in handlers GetCategoryList()")
 	res := make([]Category, 0, 100)
-	categories, err := handlers.repo.GetCategoryList(ctx)
+	categories, err := handlers.usecase.GetCategoryList(ctx)
 	if err != nil {
 		return res, err
 	}
--- internal/handlers/item_handlers.go
+++ internal/handlers/item_handlers.go
@@ -2,39 +2,57 @@ package handlers
 
 import (
 	"OnlineShopBackend/internal/models"
+	"OnlineShopBackend/internal/usecase"
 	"context"
 	"fmt"
 
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
+
+var _ IItemHandlers = &ItemHandlers{}
+
+type ItemHandlers struct {
+	usecase usecase.IItemUsecase
+	logger  *zap.Logger
+}
+
+func NewItemHandlers(usecase usecase.IItemUsecase, logger *zap.Logger) *ItemHandlers {
+	return &ItemHandlers{usecase: usecase, logger: logger}
+}
 
 // Item is struct for DTO
 type Item struct {
-	Id          string `json:"id,omitempty"`
-	Title       string `json:"title,omitempty"`
-	Description string `json:"description,omitempty"`
-	Price       int32  `json:"price,omitempty"`
-	Category    string `json:"category,omitempty"`
-	Vendor      string `json:"vendor,omitempty"`
-	Image       string `json:"image,omitempty"`
+	Id          string   `json:"id,omitempty"`
+	Title       string   `json:"title,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Price       int32    `json:"price,omitempty"`
+	Category    Category `json:"category,omitempty"`
+	Vendor      string   `json:"vendor,omitempty"`
+	Images      []string `json:"image,omitempty"`
 }
 
 // CreateItem transform Item to models.Item and call usecase CreateItem
-func (handlers *Handlers) CreateItem(ctx context.Context, item Item) (uuid.UUID, error) {
+func (handlers *ItemHandlers) CreateItem(ctx context.Context, item Item) (uuid.UUID, error) {
 	handlers.logger.Debug("Enter in handlers CreateItem()")
-	categoryId, err := uuid.Parse(item.Category)
+	categoryId, err := uuid.Parse(item.Category.Id)
 	if err != nil {
-		return uuid.UUID{}, err
+		return uuid.Nil, err
+	}
+	itemCategory := models.Category{
+		Id:          categoryId,
+		Name:        item.Category.Name,
+		Description: item.Category.Description,
 	}
 	newItem := &models.Item{
 		Title:       item.Title,
 		Description: item.Description,
 		Price:       item.Price,
-		Category:    categoryId,
+		Category:    itemCategory,
 		Vendor:      item.Vendor,
-		Image:       item.Image,
+		Images:      item.Images,
 	}
-	id, err := handlers.repo.CreateItem(ctx, newItem)
+	id, err := handlers.usecase.CreateItem(ctx, newItem)
 	if err != nil {
 		return id, err
 	}
@@ -42,85 +60,104 @@ func (handlers *Handlers) CreateItem(ctx context.Context, item Item) (uuid.UUID,
 }
 
 // UpdateItem transform Item to models.Item and call usecase UpdateItem
-func (handlers *Handlers) UpdateItem(ctx context.Context, item Item) error {
+func (handlers *ItemHandlers) UpdateItem(ctx context.Context, item Item) error {
 	handlers.logger.Debug("Enter in handlers UpdateItem()")
 	id, err := uuid.Parse(item.Id)
 	if err != nil {
 		return fmt.Errorf("invalid item uuid: %w", err)
 	}
-	categoryId, err := uuid.Parse(item.Category)
+	categoryId, err := uuid.Parse(item.Category.Id)
 	if err != nil {
 		return fmt.Errorf("invalid category uuid: %w", err)
 	}
+	itemCategory := models.Category{
+		Id:          categoryId,
+		Name:        item.Category.Name,
+		Description: item.Category.Description,
+	}
+
 	updateItem := &models.Item{
 		Id:          id,
 		Title:       item.Title,
 		Description: item.Description,
 		Price:       item.Price,
-		Category:    categoryId,
+		Category:    itemCategory,
 		Vendor:      item.Vendor,
-		Image:       item.Image,
+		Images:      item.Images,
 	}
-	return handlers.repo.UpdateItem(ctx, updateItem)
+	return handlers.usecase.UpdateItem(ctx, updateItem)
 }
 
 // GetItem returns Item on id
-func (handlers *Handlers) GetItem(ctx context.Context, id string) (Item, error) {
+func (handlers *ItemHandlers) GetItem(ctx context.Context, id string) (Item, error) {
 	handlers.logger.Debug("Enter in handlers GetItem()")
 	uid, err := uuid.Parse(id)
 	if err != nil {
 		return Item{}, fmt.Errorf("invalid item uuid: %w", err)
 	}
-	item, err := handlers.repo.GetItem(ctx, uid)
+	item, err := handlers.usecase.GetItem(ctx, uid)
 	if err != nil {
 		return Item{}, err
 	}
+
+	handlersCategory := Category{
+		Id:          item.Category.Id.String(),
+		Name:        item.Category.Name,
+		Description: item.Category.Description,
+	}
+
 	return Item{
 		Id:          item.Id.String(),
 		Title:       item.Title,
 		Description: item.Description,
 		Price:       item.Price,
-		Category:    item.Category.String(),
+		Category:    handlersCategory,
 		Vendor:      item.Vendor,
-		Image:       item.Image,
+		Images:      item.Images,
 	}, nil
 }
 
 // ItemsList returns list of all Items
-func (handlers *Handlers) ItemsList(ctx context.Context) ([]Item, error) {
+func (handlers *ItemHandlers) ItemsList(ctx context.Context, offset, limit int) ([]Item, error) {
 	handlers.logger.Debug("Enter in handlers ItemsList()")
-	res := make([]Item, 0, 100)
-	items, err := handlers.repo.ItemsList(ctx)
+	res := make([]Item, 0, limit)
+	items, err := handlers.usecase.ItemsList(ctx, offset, limit)
 	if err != nil {
 		return res, err
 	}
-	for {
-		select {
-		case <-ctx.Done():
-			handlers.logger.Debug("handlers ItemList() ctx.Done recieved")
-			return res, ctx.Err()
-		case item, ok := <-items:
-			if !ok {
-				return res, nil
-			}
-			res = append(res, Item{
-				Id:          item.Id.String(),
-				Title:       item.Title,
-				Description: item.Description,
-				Price:       item.Price,
-				Category:    item.Category.String(),
-				Vendor:      item.Vendor,
-			})
+	for _, item := range items {
+		handlersCategory := Category{
+			Id:          item.Category.Id.String(),
+			Name:        item.Category.Name,
+			Description: item.Category.Description,
 		}
-	}
 
+		res = append(res, Item{
+			Id:          item.Id.String(),
+			Title:       item.Title,
+			Description: item.Description,
+			Price:       item.Price,
+			Category:    handlersCategory,
+			Vendor:      item.Vendor,
+		})
+	}
+	return res, nil
+}
+
+func (handlers *ItemHandlers) ItemsQuantity(ctx context.Context) (int, error) {
+	handlers.logger.Debug("Enter in handlers ItemsQuantity()")
+	quantity, err := handlers.usecase.ItemsQuantity(ctx)
+	if err != nil {
+		return quantity, err
+	}
+	return quantity, nil
 }
 
 // SearchLine returns list of Items with parameters
-func (handlers *Handlers) SearchLine(ctx context.Context, param string) ([]Item, error) {
+func (handlers *ItemHandlers) SearchLine(ctx context.Context, param string) ([]Item, error) {
 	handlers.logger.Debug("Enter in handlers SearchLine()")
 	res := make([]Item, 0, 100)
-	items, err := handlers.repo.SearchLine(ctx, param)
+	items, err := handlers.usecase.SearchLine(ctx, param)
 	if err != nil {
 		return res, err
 	}
@@ -133,12 +170,17 @@ func (handlers *Handlers) SearchLine(ctx context.Context, param string) ([]Item,
 			if !ok {
 				return res, nil
 			}
+			handlersCategory := Category{
+				Id:          item.Category.Id.String(),
+				Name:        item.Category.Name,
+				Description: item.Category.Description,
+			}
 			res = append(res, Item{
 				Id:          item.Id.String(),
 				Title:       item.Title,
 				Description: item.Description,
 				Price:       item.Price,
-				Category:    item.Category.String(),
+				Category:    handlersCategory,
 				Vendor:      item.Vendor,
 			})
 		}
