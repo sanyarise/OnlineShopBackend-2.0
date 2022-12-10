3e097823e6eb7877b8d0a38ac5050f823e55cd82
sanyarise <san_rise@list.ru>
2022-12-10 21:56:37 +0300

    feat:add usecase interface and update usecase

--- internal/usecase/category_usecase.go
+++ internal/usecase/category_usecase.go
@@ -2,16 +2,29 @@ package usecase
 
 import (
 	"OnlineShopBackend/internal/models"
+	"OnlineShopBackend/internal/repository"
 	"context"
 	"fmt"
 
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
 
-// CreateCategory call database method and returns id of created category or error
-func (storage *Storage) CreateCategory(ctx context.Context, category *models.Category) (uuid.UUID, error) {
-	storage.logger.Debug("Enter in usecase CreateCategory()")
-	id, err := storage.categoryStore.CreateCategory(ctx, category)
+var _ ICategoryUsecase = &CategoryUsecase{}
+
+type CategoryUsecase struct {
+	categoryStore repository.CategoryStore
+	logger        *zap.Logger
+}
+
+func NewCategoryUsecase(store repository.CategoryStore, logger *zap.Logger) ICategoryUsecase {
+	return &CategoryUsecase{categoryStore: store, logger: logger}
+}
+
+// / CreateCategory call database method and returns id of created category or error
+func (usecase *CategoryUsecase) CreateCategory(ctx context.Context, category *models.Category) (uuid.UUID, error) {
+	usecase.logger.Debug("Enter in usecase CreateCategory()")
+	id, err := usecase.categoryStore.CreateCategory(ctx, category)
 	if err != nil {
 		return uuid.Nil, fmt.Errorf("error on create category: %w", err)
 	}
@@ -19,9 +32,9 @@ func (storage *Storage) CreateCategory(ctx context.Context, category *models.Cat
 }
 
 // GetCategoryList call database method and returns chan with all models.Category or error
-func (storage *Storage) GetCategoryList(ctx context.Context) (chan models.Category, error) {
-	storage.logger.Debug("Enter in usecase GetCategoryList()")
-	categoryIncomingChan, err := storage.categoryStore.GetCategoryList(ctx)
+func (usecase *CategoryUsecase) GetCategoryList(ctx context.Context) (chan models.Category, error) {
+	usecase.logger.Debug("Enter in usecase GetCategoryList()")
+	categoryIncomingChan, err := usecase.categoryStore.GetCategoryList(ctx)
 	if err != nil {
 		return nil, err
 	}
