0ab8f209cf3bf54fed568489113e0044efc3ad94
sanyarise <san_rise@list.ru>
2022-12-10 22:10:20 +0300

    feat: add handlers item and category interfaces

---
+++ internal/handlers/handlers_interface.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type IItemHandlers interface {
+	CreateItem(ctx context.Context, item Item) (uuid.UUID, error)
+	UpdateItem(ctx context.Context, item Item) error
+	GetItem(ctx context.Context, id string) (Item, error)
+	ItemsList(ctx context.Context, offset, limit int) ([]Item, error)
+	ItemsQuantity(ctx context.Context) (int, error)
+	SearchLine(ctx context.Context, param string) ([]Item, error)
+}
+
+type ICategoryHandlers interface {
+	CreateCategory(ctx context.Context, category Category) (uuid.UUID, error)
+	GetCategoryList(ctx context.Context) ([]Category, error)
+}
