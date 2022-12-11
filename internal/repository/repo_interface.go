ec2af1b0533895be188daff85287ef317a9801f0
sanyarise <san_rise@list.ru>
2022-12-11 22:01:54 +0300

    feat: add GetCategory methods

--- internal/repository/repo_interface.go
+++ internal/repository/repo_interface.go
@@ -21,6 +21,7 @@ type CategoryStore interface {
 	UpdateCategory(ctx context.Context, category *models.Category) error
 	GetCategory(ctx context.Context, id uuid.UUID) (*models.Category, error)
 	GetCategoryList(ctx context.Context) (chan models.Category, error)
+	GetCategory(ctx context.Context, id uuid.UUID) (*models.Category, error)
 }
 
 type UserStore interface {
