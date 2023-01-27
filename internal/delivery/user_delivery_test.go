349474f7f76239fd3090d47cc4ef3f5e9dbeb45c
Alexandr Pilyuk <79321531+sanyarise@users.noreply.github.com>
2023-01-27 16:08:50 +0300

    feat: add tests on create rights

--- internal/delivery/user_delivery_test.go
+++ internal/delivery/user_delivery_test.go
@@ -54,4 +54,118 @@ func TestCreateUser(t *testing.T) {
 
 }*/
 
+import (
+	"OnlineShopBackend/internal/delivery/user"
+	fs "OnlineShopBackend/internal/filestorage/mocks"
+	"OnlineShopBackend/internal/models"
+	"OnlineShopBackend/internal/usecase/mocks"
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+type WrongShortRights struct {
+	Name int
+}
+type WrongRights struct {
+	Id   int
+	Name int
+}
+
+var (
+	testShortRights = user.ShortRights{
+		Name: "test",
+	}
+	testModelRightsNoId = models.Rights{
+		Name: "test",
+	}
+	testSREmptyName = user.ShortRights{
+		Name: "",
+	}
+	testWrongShortRights = WrongShortRights{
+		Name: 5,
+	}
+)
+
+func MockRightsJson(c *gin.Context, content interface{}, method string) {
+	if method == "POST" {
+		c.Request.Method = "POST"
+	}
+	if method == "PUT" {
+		c.Request.Method = "PUT"
+	}
+
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	jsonbytes, err := json.Marshal(content)
+	if err != nil {
+		panic(err)
+	}
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(jsonbytes))
+}
+
+func TestCreateRights(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	logger := zap.L()
+	itemUsecase := mocks.NewMockIItemUsecase(ctrl)
+	categoryUsecase := mocks.NewMockICategoryUsecase(ctrl)
+	userUsecase := mocks.NewMockIUserUsecase(ctrl)
+	cartUsecase := mocks.NewMockICartUsecase(ctrl)
+	filestorage := fs.NewMockFileStorager(ctrl)
+	orderUsecase := mocks.NewMockIOrderUsecase(ctrl)
+	delivery := NewDelivery(itemUsecase, userUsecase, categoryUsecase, cartUsecase, logger, filestorage, orderUsecase)
+	ctx := context.Background()
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+	MockRightsJson(c, testWrongShortRights, post)
+	delivery.CreateRights(c)
+	require.Equal(t, 400, w.Code)
+
+	w = httptest.NewRecorder()
+	c, _ = gin.CreateTestContext(w)
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+	MockRightsJson(c, testSREmptyName, post)
+	delivery.CreateRights(c)
+	require.Equal(t, 400, w.Code)
+
+	w = httptest.NewRecorder()
+	c, _ = gin.CreateTestContext(w)
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+	MockRightsJson(c, testShortRights, post)
+	userUsecase.EXPECT().CreateRights(ctx, &testModelRightsNoId).Return(uuid.Nil, err)
+	delivery.CreateRights(c)
+	require.Equal(t, 500, w.Code)
+
+	w = httptest.NewRecorder()
+	c, _ = gin.CreateTestContext(w)
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+	MockRightsJson(c, testShortRights, post)
+	userUsecase.EXPECT().CreateRights(ctx, &testModelRightsNoId).Return(testId, nil)
+	delivery.CreateRights(c)
+	require.Equal(t, 201, w.Code)
+}
